952e282556ef367a53f8d5cd84cd03f7fb177a03
leekjoo <leekjoo1008@gmail.com>
2024-11-29 00:49:05 +0900

    style: comment-form

--- board-project/src/components/nav.jsx
+++ board-project/src/components/nav.jsx
@@ -1,12 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useDarkMode } from "../context/darkModeContext";
-import { IoMoon } from "react-icons/io5";
-import { MdWbSunny } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+
 import Button from "./button";
 import { useLogin } from "../context/loginContext";
 
 const Nav = () => {
-  const { isDarkMode, toggleDarkMode } = useDarkMode();
   const { isLogin, handleLogin, userData } = useLogin();
   const { userName } = userData;
   const handleLogout = () => {
@@ -21,16 +18,12 @@ const Nav = () => {
         </a>
         {/* 로그인이 돼있으면 유저 프로필 버튼 보여주기 */}
         <div className="nav-right">
-          <button className="darkModeBtn" onClick={toggleDarkMode}>
-            {!isDarkMode && <IoMoon />}
-            {isDarkMode && <MdWbSunny />}
-          </button>
+          {isLogin && (
+            <span className="user-name">{`반가워요 ${userName} 님`}</span>
+          )}
           {isLogin && <Button text="로그아웃" onClick={handleLogout}></Button>}
           {!isLogin && (
             <Button text="Login" onClick={() => navigate("login")} />
-          )}
-          {isLogin && (
-            <span className="user-name">{`반가워요 ${userName} 님`}</span>
           )}
         </div>
       </nav>
--- board-project/src/pages/post-detail.jsx
+++ board-project/src/pages/post-detail.jsx
@@ -4,6 +4,8 @@ import { usePosts } from "../context/postContext";
 import { useLogin } from "../context/loginContext";
 import Comment from "../components/comment";
 import Edit from "./edit";
+import { FaRegCommentDots } from "react-icons/fa";
+import Button from "../components/button";
 
 const PostDetail = () => {
   const location = useLocation();
@@ -57,50 +59,61 @@ const PostDetail = () => {
   };
 
   return (
-    <>
-      <div>
-        <span>{userName}</span>
-        <h3>{title}</h3>
-        {isLogin && userData.userId == post.userId ? (
-          <span className="btns-wrap">
-            <button onClick={() => toggleEditPost(post)}>수정하기</button>
-            <button onClick={() => handleDelete(post)}>삭제하기</button>
-          </span>
-        ) : (
-          ""
+    <div className="container detail-container">
+      <div className="detail-inner">
+        <div className="post-detail">
+          <div className="post-header">
+            <h3 className="main-post-title">{title}</h3>
+            <span className="main-user-name">{userName}</span>
+
+            {isLogin && userData.userId == post.userId ? (
+              <span className="btns-wrap">
+                <button onClick={() => toggleEditPost(post)}>수정하기</button>
+                <button onClick={() => handleDelete(post)}>삭제하기</button>
+              </span>
+            ) : (
+              ""
+            )}
+          </div>
+          <p className="main-post-content">{content}</p>
+
+          <div className="comment">
+            <div className="comment-line">
+              <FaRegCommentDots className="comment-text" />
+              댓글
+            </div>
+            {isLogin && (
+              <form onSubmit={handleSubmit} className="comment-form">
+                <span className="user-name">{userData.userName}</span>
+                <input
+                  className="comment-input"
+                  type="text"
+                  name="reply"
+                  value={reply}
+                  placeholder="댓글을 남겨보세요"
+                  onChange={(e) => setReply(e.target.value)}
+                ></input>
+                <Button text="등록" className="comment-submit" />
+              </form>
+            )}
+            <ul>
+              {comments.map((reply) => (
+                <Comment reply={reply} />
+              ))}
+            </ul>
+          </div>
+        </div>
+        {isEditing && (
+          <Edit
+            posts={posts}
+            toggleEditPost={toggleEditPost}
+            updatedPost={updatedPost}
+            editingPost={editingPost}
+            setIsEditing={setIsEditing}
+          />
         )}
       </div>
-      <p>{content}</p>
-      <div className="comment">
-        <span>댓글</span>
-        {isLogin && (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="reply"
-              value={reply}
-              placeholder="댓글을 남겨주세요"
-              onChange={(e) => setReply(e.target.value)}
-            ></input>
-            <button>입력</button>
-          </form>
-        )}
-        <ul>
-          {comments.map((reply) => (
-            <Comment reply={reply} />
-          ))}
-        </ul>
-      </div>
-      {isEditing && (
-        <Edit
-          posts={posts}
-          toggleEditPost={toggleEditPost}
-          updatedPost={updatedPost}
-          editingPost={editingPost}
-          setIsEditing={setIsEditing}
-        />
-      )}
-    </>
+    </div>
   );
 };
 export default PostDetail;
